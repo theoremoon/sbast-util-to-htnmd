2ab2bf490c5b2a1a14730a142ad22f1c932301ab
theoremoon <theoldmoon0602@gmail.com>
2023-11-20 00:05:55 +0900

    add decoration patterns

--- src/decorationHandlers/index.ts
+++ src/decorationHandlers/index.ts
@@ -1,22 +1,10 @@
-import { heading, paragraph, text } from "npm:mdast-builder";
+import { emphasis, heading, strike } from "npm:mdast-builder";
 import type { Node } from "npm:@types/mdast";
 
-function generateHandler(deco: string) {
-  switch (deco) {
-    case "*-3":
-      return (children: Node[]) => heading(2, children);
-    case "*-2":
-      return (children: Node[]) => heading(3, children);
-    case "*-1":
-      return (children: Node[]) => heading(4, children);
-    default:
-      return (children: Node[]) =>
-        paragraph([text(`deco:[${deco}]`), ...children]);
-  }
-}
-
 export const decorationHandlers = {
-  "*-3": generateHandler("*-3"),
-  "*-2": generateHandler("*-2"),
-  "*-1": generateHandler("*-1"),
+  "*-3": (children: Node[]) => heading(2, children),
+  "*-2": (children: Node[]) => heading(3, children),
+  "*-1": (children: Node[]) => heading(4, children),
+  "-": (children: Node[]) => strike(children),
+  "/": (children: Node[]) => emphasis(children),
 };
