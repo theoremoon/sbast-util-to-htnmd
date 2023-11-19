8e6495f8ee0d843e88eba52d554ba56679c78d94
theoremoon <theoldmoon0602@gmail.com>
2023-11-20 00:24:10 +0900

    stop to support delete

--- src/decorationHandlers/index.ts
+++ src/decorationHandlers/index.ts
@@ -1,10 +1,10 @@
-import { emphasis, heading, strike } from "npm:mdast-builder";
+import { emphasis, heading, strong } from "npm:mdast-builder";
 import type { Node } from "npm:@types/mdast";
 
 export const decorationHandlers = {
   "*-3": (children: Node[]) => heading(2, children),
   "*-2": (children: Node[]) => heading(3, children),
   "*-1": (children: Node[]) => heading(4, children),
-  "-": (children: Node[]) => strike(children),
+  "*": (children: Node[]) => strong(children),
   "/": (children: Node[]) => emphasis(children),
 };
